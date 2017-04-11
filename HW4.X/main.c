8f46f15053768f4c52454a73c8e4901e95a38051
spartycubs <LoganSpringgate2018@u.northwestern.edu>
2017-04-11 05:56:58 -0500

    Debugging changes

--- HW4.X/main.c
+++ HW4.X/main.c
@@ -8,42 +8,6 @@
 static volatile float sine_wave[200];
 static volatile float tri_wave[200];   // Define vectors for waveforms
 
-int main(void) {
-    init_spi1();
-    waveforms();
-    while(1) {
-        static int counter = 0;
-        if (_CP0_GET_COUNT() >= 24000) {        // Cycle 1000x per second
-            _CP0_SET_COUNT(0);                  // Reset count
-            setVoltage(0,sine_wave[counter]);
-            setVoltage(1,tri_wave[counter]);    // Set voltages for each Vout
-            if (counter == 199) {
-                counter = 0;                    // Reset wave counters
-            }
-        }
-    }
-    return 0;
-}
-
-void init_spi1() {
-    // set up the chip select pin as an output
-    TRISBbits.TRISB8 = 0;
-    CS = 1;
-        
-    SS1Rbits.SS1R = 0b0011     // Set B15 as the SS1 input pin
-    SDI1Rbits.SDI1R = 0b0100   // Set B8 as the SDI1 input pin
-    RPB13Rbits.RPB13R = 0b0011 // Set B13 as the SDO1 output pin
-    
-    SPI1BUF;                   // Clear buffer by reading from it
-    SPI1BRG = 0x1;             // Baud rate is 12 MHz [SPI1BRG = (48000000/(2*desired))-1]
-    SPI1STATbits.SPIROV = 0;   // clear the overflow bit
-    SPI1CONbits.MODE32 = 0;
-    SPI1CONbits.MODE16 = 0;    // Use 8 bit mode
-    SPI1CONbits.MSTEN = 1;     // master operation
-    SPI1CONbits.ON = 1;        // Turn on SPI1
-    
-}
-
 void waveforms(void) {
     static int i;
     for (i=0; i<200; i++) {
@@ -67,4 +31,40 @@ void setVoltage (char channel, float voltage) {
     CS = 0;
     spi_io((channel << 15) | 0x7000 | (convert << 4));  // Setting SPI1BUF from MCP4902 data sheet
     CS = 1;
+}
+
+void init_spi1() {
+    // set up the chip select pin as an output
+    TRISBbits.TRISB8 = 0;
+    CS = 1;
+        
+    SS1Rbits.SS1R = 0b0011;     // Set B15 as the SS1 input pin
+    SDI1Rbits.SDI1R = 0b0100;   // Set B8 as the SDI1 input pin
+    RPB13Rbits.RPB13R = 0b0011; // Set B13 as the SDO1 output pin
+    
+    SPI1BUF;                   // Clear buffer by reading from it
+    SPI1BRG = 0x1;             // Baud rate is 12 MHz [SPI1BRG = (48000000/(2*desired))-1]
+    SPI1STATbits.SPIROV = 0;   // clear the overflow bit
+    SPI1CONbits.MODE32 = 0;
+    SPI1CONbits.MODE16 = 0;    // Use 8 bit mode
+    SPI1CONbits.MSTEN = 1;     // master operation
+    SPI1CONbits.ON = 1;        // Turn on SPI1
+    
+}
+
+int main(void) {
+    init_spi1();
+    waveforms();
+    while(1) {
+        static int counter = 0;
+        if (_CP0_GET_COUNT() >= 24000) {        // Cycle 1000x per second
+            _CP0_SET_COUNT(0);                  // Reset count
+            setVoltage(0,sine_wave[counter]);
+            setVoltage(1,tri_wave[counter]);    // Set voltages for each Vout
+            if (counter == 199) {
+                counter = 0;                    // Reset wave counters
+            }
+        }
+    }
+    return 0;
 }
