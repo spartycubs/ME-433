a5adf3997fd4ce7d7ba948ae52b29e6a51b6df97
spartycubs <LoganSpringgate2018@u.northwestern.edu>
2017-04-18 18:08:19 -0500

    Began LCD homework

---
+++ HW6.X/main.c
@@ -0,0 +1,24 @@
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "library.h"
+
+int main(void) {
+    __builtin_disable_interrupts();
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+    TRISAbits.TRISA4 = 0;
+    TRISBbits.TRISB4 = 1;
+
+    __builtin_enable_interrupts();
+    return 0;
+}
+
