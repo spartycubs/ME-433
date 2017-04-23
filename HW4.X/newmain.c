d4520a0bb1d39a76265d80e07d44a7c02725fa29
spartycubs <LoganSpringgate2018@u.northwestern.edu>
2017-04-23 17:46:44 -0500

    Still not functional

--- HW4.X/newmain.c
+++ HW4.X/newmain.c
@@ -21,52 +21,74 @@ void init_spi1() {
     SPI1BUF;                    // Clear buffer by reading from it
     SPI1BRG = 0x1000;             // Baud rate is 12 MHz [SPI1BRG = (48000000/(2*desired))-1]
     SPI1STATbits.SPIROV = 0;    // clear the overflow bit
-    SPI1CONbits.MODE32 = 0;
-    SPI1CONbits.MODE16 = 0;     // Use 8 bit mode
+    //SPI1CONbits.MODE32 = 0;
+    //SPI1CONbits.MODE16 = 0;     // Use 8 bit mode
     SPI1CONbits.CKE = 1;
     SPI1CONbits.MSTEN = 1;      // master operation
+    SPI1CONbits.SSEN = 0;
     SPI1CONbits.ON = 1;         // Turn on SPI1
 }
 
 void waveforms(void) {
-    static int i;
+    int i;
     for (i=0; i<200; i++) {
         float temp;
-        temp = 127.5+127.5*sin(2.0*3.14159*10*(float) i/200.0);  // 10 Hz sine wave
-        sine_wave[i] = (unsigned char) temp;
-        temp = 255.0*(float) i/200.0;                     // 5 Hz triangle wave
+        temp = 127.5 + 127.5* sin(2 * 3.14159*10* (i /200.0));
+        sine_wave[i] =  (unsigned char) temp;
+        temp = 255.0 * i/200.0;                     // 5 Hz triangle wave
         tri_wave[i] = (unsigned char) temp;
     }
 }
 
 // send a byte via spi and return the response
 unsigned char spi_io(unsigned char o) {
-  SPI1BUF = o;
-  while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
-    ;
-  }
-  return SPI1BUF;
+    SPI1BUF = o;
+    while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
+        ;
+    }
+    return SPI1BUF;
 }
 
-void setVoltage (char channel, float voltage) {
-    int convert = voltage;                              // Make float voltage into int convert
+void setVoltage (char channel, unsigned char volt) {
     CS = 0;
-    spi_io((channel << 15) | 0x7000 | (convert << 4));  // Setting SPI1BUF from MCP4902 data sheet
+    spi_io((channel << 15) | 0x7000 | (volt >> 4));  // Setting SPI1BUF from MCP4902 data sheet
+    _CP0_SET_COUNT(0);
+    while (_CP0_GET_COUNT()<2400){;}
+    spi_io(volt << 4);
     CS = 1;
 }
 
 int main(void) {
+    
+    __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+
+    __builtin_enable_interrupts();
+    
     init_spi1();
     waveforms();
     while(1) {
         static int counter = 0;
         if (_CP0_GET_COUNT() >= 24000) {        // Cycle 1000x per second
-            _CP0_SET_COUNT(0);                  // Reset count
             setVoltage(0,sine_wave[counter]);
             setVoltage(1,tri_wave[counter]);    // Set voltages for each Vout
             if (counter == 199) {
                 counter = 0;                    // Reset wave counters
             }
+            counter++;
         }
     }
     return 0;
